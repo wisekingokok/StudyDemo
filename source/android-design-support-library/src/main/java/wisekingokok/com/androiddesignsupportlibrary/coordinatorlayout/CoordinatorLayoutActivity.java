802ddfdd263bc52dceb3a8eba31e09f94e16bea3
linyanjun <linyanjun@danlu.com>
2017-02-07 10:53:42 +0800

    增加CoordinatorLayout

--- source/android-design-support-library/src/main/java/wisekingokok/com/androiddesignsupportlibrary/coordinatorlayout/CoordinatorLayoutActivity.java
+++ source/android-design-support-library/src/main/java/wisekingokok/com/androiddesignsupportlibrary/coordinatorlayout/CoordinatorLayoutActivity.java
@@ -1,7 +1,11 @@
 package wisekingokok.com.androiddesignsupportlibrary.coordinatorlayout;
 
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
 import wisekingokok.com.androiddesignsupportlibrary.R;
 
@@ -16,7 +20,8 @@ import wisekingokok.com.androiddesignsupportlibrary.R;
  */
 public class CoordinatorLayoutActivity extends AppCompatActivity {
 
-
+    private int lastX;
+    private int lastY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +29,43 @@ public class CoordinatorLayoutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_coordinator_layout);
 
 
+        TextView tv = (TextView) findViewById(R.id.tv);
+        tv.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int x = (int) event.getRawX();
+                int y = (int) event.getRawY();
+                switch (event.getAction()) {
+
+                    case MotionEvent.ACTION_DOWN: {
+
+                        break;
+                    }
+
+                    case MotionEvent.ACTION_MOVE: {
+                        CoordinatorLayout.MarginLayoutParams layoutParams = (CoordinatorLayout.MarginLayoutParams) v.getLayoutParams();
+                        int left = layoutParams.leftMargin + x - lastX;
+                        int top = layoutParams.topMargin + y - lastY;
 
 
+                        layoutParams.leftMargin = left;
+                        layoutParams.topMargin = top;
+                        v.setLayoutParams(layoutParams);
+                        v.requestLayout();
+                        break;
+                    }
 
+                    case MotionEvent.ACTION_UP: {
 
+                        break;
+                    }
+
+                }
+                lastX = x;
+                lastY = y;
+                return true;
+            }
+        });
     }
 
 }
